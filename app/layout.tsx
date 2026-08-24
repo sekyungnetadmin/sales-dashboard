ca9cef421187d390b96ffcc6ba71563be8b64aa1
sekyungnetadmin <info@sekyungtex.co.kr>
2026-08-24 12:12:07 +0900

    re

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,19 @@ import ConsentGate from '@/components/ConsentGate';
 export const metadata: Metadata = {
   title: "세경 매출 대시보드",
   description: "거래처별 매출 분석 대시보드",
+   manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+       <head>
+  <link rel="manifest" href="/manifest.json" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+</head>
       </head>
       <body>
          <ConsentGate>{children}</ConsentGate>
